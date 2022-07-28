3d8aa0d02cf4d6bd9757f9bba8f2d2ff09579d41
Matthew Zhigalo <matthewzhigalo@gmail.com>
2022-07-28 15:49:17 +0300

    solved

--- src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
+++ src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
@@ -5,16 +5,14 @@ import java.util.OptionalInt;
 public class MaxMethod {
     public static OptionalInt max(int[] values) {
         int max;
-        OptionalInt maxVal;
         if (values != null && values.length > 0) {
             max = values[0];
             for (int value : values)
                 if (max < value)
                     max = value;
-            maxVal = OptionalInt.of(max);
+            return OptionalInt.of(max);
         } else {
-            maxVal = OptionalInt.empty();
+            return OptionalInt.empty();
         }
-        return maxVal;
     }
 }
